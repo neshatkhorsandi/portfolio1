72cb3edc89010d5205b8eec621f8836c3eddb93d
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-06-01 13:46:32 -0600

    got rid of motion div for projects

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
-import { motion } from "framer-motion";
-
+import { client, urlFor } from "../../client";
 import { AppWrap, MotionWrap } from "../../wrapper";
-import { urlFor, client } from "../../client";
 import "./Work.scss";
 
 const Work = () => {
@@ -57,46 +55,24 @@ const Work = () => {
         ))}
       </div>
 
-      <motion.div
-        animate={animateCard}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__work-portfolio"
-      >
+      <div className="app__work-portfolio">
         {filterWork.map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div className="app__work-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{
-                  duration: 0.25,
-                  ease: "easeInOut",
-                  staggerChildren: 0.5,
-                }}
-                className="app__work-hover app__flex"
-              >
+              <div className="app__work-hover app__flex">
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
+                  <div className="app__flex">
                     <AiFillEye />
-                  </motion.div>
+                  </div>
                 </a>
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
+                  <div className="app__flex">
                     <AiFillGithub />
-                  </motion.div>
+                  </div>
                 </a>
-              </motion.div>
+              </div>
             </div>
 
             <div className="app__work-content app__flex">
@@ -111,7 +87,7 @@ const Work = () => {
             </div>
           </div>
         ))}
-      </motion.div>
+      </div>
     </>
   );
 };
