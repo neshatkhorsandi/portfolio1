d96f8d0be71520191a4263295c9d01164fdf629e
Neshat Khorsandi <neshat.khorsandi@edu.sait.ca>
2022-08-04 18:54:00 -0600

    got rid of ReactJs as a project filter

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { AiFillEye, AiFillGithub } from "react-icons/ai";
-import { client, urlFor } from "../../client";
-import { AppWrap, MotionWrap } from "../../wrapper";
-import "./Work.scss";
+import React, { useEffect, useState } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { client, urlFor } from '../../client';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './Work.scss';
 
 const Work = () => {
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
-  const [activeFilter, setActiveFilter] = useState("All");
+  const [activeFilter, setActiveFilter] = useState('All');
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
@@ -27,7 +27,7 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === "All") {
+      if (item === 'All') {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(item)));
@@ -37,17 +37,17 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">
+      <h2 className='head-text'>
         My Creative <span>Portfolio</span> Section
       </h2>
 
-      <div className="app__work-filter">
-        {["Web App", "React JS", "Full Stack", "All"].map((item, index) => (
+      <div className='app__work-filter'>
+        {['Web App', 'Full Stack', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
             className={`app__work-filter-item app__flex p-text ${
-              activeFilter === item ? "item-active" : ""
+              activeFilter === item ? 'item-active' : ''
             }`}
           >
             {item}
@@ -55,34 +55,34 @@ const Work = () => {
         ))}
       </div>
 
-      <div className="app__work-portfolio">
+      <div className='app__work-portfolio'>
         {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex" key={index}>
-            <div className="app__work-img app__flex">
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
-              <div className="app__work-hover app__flex">
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <div className="app__flex">
+              <div className='app__work-hover app__flex'>
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                  <div className='app__flex'>
                     <AiFillEye />
                   </div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <div className="app__flex">
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                  <div className='app__flex'>
                     <AiFillGithub />
                   </div>
                 </a>
               </div>
             </div>
 
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{ marginTop: 10 }}>
                 {work.description}
               </p>
 
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
               </div>
             </div>
           </div>
@@ -93,7 +93,7 @@ const Work = () => {
 };
 
 export default AppWrap(
-  MotionWrap(Work, "app__works"),
-  "work",
-  "app__primarybg"
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg'
 );
